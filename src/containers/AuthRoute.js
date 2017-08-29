2a0002e5e899c0c6b8d17ab8d08a48b13a395d22
Scott Savage <snsavage@gmail.com>
2017-08-29 13:26:28 -0400

    Remove AuthRoute container

--- src/containers/AuthRoute.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
-
-class AuthRoute extends Component {
-  render() {
-  }
-}
