26fe57597ffa1de7a2a9ddc4c542d68ea3164bc5
Scott Savage <snsavage@gmail.com>
2017-08-31 11:12:45 -0400

    Style Register and SignIn

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -5,6 +5,8 @@ import { withRouter } from 'react-router-dom';
 
 import * as actions from '../actions/sessionActions';
 
+import { Button, Grid, Form, Header } from 'semantic-ui-react';
+
 class Register extends Component {
   constructor(props) {
     super(props);
@@ -26,41 +28,49 @@ class Register extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Register</h1>
-        <form onChange={(ev) => this.onChange(ev)} >
-          <div>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={this.state.name} />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={this.state.email} />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={this.state.password} />
-          </div>
-          <div>
-            <input
+      <Grid
+        verticalAlign="middle"
+        centered
+        style={{
+          height: '100%',
+          margin: 0,
+        }}>
+        <Grid.Column width={6}>
+          <Header as="h1">Register</Header>
+          <Form onChange={(ev) => this.onChange(ev)} >
+            <Form.Field>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={this.state.name} />
+            </Form.Field>
+            <Form.Field>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={this.state.email} />
+            </Form.Field>
+            <Form.Field>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={this.state.password} />
+            </Form.Field>
+            <Button
+              color="blue"
               type="submit"
-              value="Sign Up"
-              onClick={(ev) => this.onSubmit(ev)} />
-          </div>
-        </form>
-      </div>
+              onClick={(ev) => this.onSubmit(ev)}>
+              Register
+            </Button>
+          </Form>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
--- src/containers/SignIn.js
+++ src/containers/SignIn.js
@@ -5,6 +5,8 @@ import { withRouter } from 'react-router-dom';
 
 import * as actions from '../actions/sessionActions';
 
+import { Button, Grid, Form, Header } from 'semantic-ui-react';
+
 class SignIn extends Component {
   constructor(props) {
     super(props);
@@ -26,33 +28,41 @@ class SignIn extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Sign In</h1>
-        <form onChange={(ev) => this.onChange(ev)} >
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={this.state.email} />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={this.state.password} />
-          </div>
-          <div>
-            <input
+      <Grid
+        verticalAlign="middle"
+        centered
+        style={{
+          height: '100%',
+          margin: 0,
+        }}>
+        <Grid.Column width={6}>
+          <Header as="h1">Sign In</Header>
+          <Form onChange={(ev) => this.onChange(ev)} >
+            <Form.Field>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={this.state.email} />
+            </Form.Field>
+            <Form.Field>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={this.state.password} />
+            </Form.Field>
+            <Button
+              color="blue"
               type="submit"
-              value="Sign In"
-              onClick={(ev) => this.onSubmit(ev)} />
-          </div>
-        </form>
-      </div>
+              onClick={(ev) => this.onSubmit(ev)}>
+              Sign In
+            </Button>
+          </Form>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
